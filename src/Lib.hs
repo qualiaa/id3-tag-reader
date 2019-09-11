61318bb893fec9e70ff6da9466a074a149203350
Jamie Bayne <jamiebayne@hotmail.com>
2019-09-11 13:50:55 +0100

    Recursively list music files

--- src/Lib.hs
+++ src/Lib.hs
@@ -2,7 +2,34 @@ module Lib
     ( someFunc
     ) where
 
+import Safe (tailSafe)
+import Data.Char (toLower)
 import System.Directory
+import System.FilePath ((</>), takeExtension)
+import Sound.HTagLib
+
+-- (a -> IO Bool) -> (a -> bool) -> (a -> b) -> a -> IO b
+musicExtensions = ["mp3", "ogg", "flac", "wav"]
+
+isMusicExtension = flip elem musicExtensions
+
+find :: [FilePath] -> IO [FilePath]
+find [] = return []
+find (path:paths) = do
+    dir <- doesDirectoryExist path
+    file <- doesFileExist path
+    let rest = find paths
+    if dir then do
+        list <- map (path </>) <$> listDirectory path
+        let rest' = (++) <$> find list <*> rest
+        (:) <$> pure path <*> rest'
+    else if file then
+        (:) <$> pure path <*> rest
+    else rest
+
+isMusicFile :: FilePath -> Bool
+isMusicFile = isMusicExtension . map toLower . tailSafe . takeExtension 
+
 
 someFunc :: FilePath -> IO ()
-someFunc rootDir = putStr =<< unlines <$> listDirectory rootDir
+someFunc rootDir = putStr =<< unlines .  filter isMusicFile <$> find [rootDir]
