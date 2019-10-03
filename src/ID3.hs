000c951f826c2670e256a15fa580db4398d713c7
Jamie Bayne <jamiebayne@hotmail.com>
2019-10-03 20:02:37 +0100

    Handle basic text frames

--- src/ID3.hs
+++ src/ID3.hs
@@ -4,10 +4,12 @@ module ID3
 
 import Prelude hiding (fail)
 import System.IO (openFile, IOMode(..))
+import Data.List (partition)
 import Data.Maybe
 import Control.Applicative ((<|>))
-import Control.Monad (when)
+import Control.Monad (when, forM_)
 import qualified Data.ByteString.Lazy as L
+import qualified Data.Text.IO as TIO
 
 import ParseBS
 import ID3.Header
@@ -15,26 +17,55 @@ import qualified ID3.V1
 import qualified ID3.V2p2
 import qualified ID3.V2p3
 
+parseID3 :: Parse [ID3.V2p2.UnparsedFrame]
+parseID3 = parseV2-- <|> parseV1
+
 parseV1 :: Parse String
 parseV1 = ID3.V1.parseTag
 
-parseV2 :: Parse String
+parseV2 :: Parse [ID3.V2p2.UnparsedFrame]
 parseV2 = do
     header <- parseHeader
     case id3Version header of
         (2,_) -> ID3.V2p2.parseTag header
-        (3,_) -> ID3.V2p3.parseTag header
-        otherwise -> fail "Unhandled version"
+        --(3,_) -> ID3.V2p3.parseTag header
+        otherwise -> return []
 
-parseID3 :: Parse String
-parseID3 = parseV2 <|> parseV1
+
+handleFrames :: [ID3.V2p2.UnparsedFrame] -> IO ()
+handleFrames frames = do
+    let (textFrames, nontext) = partition (('T'==).head.fst.fst) frames
+        nontextIDs = map (fst.fst) nontext
+
+    putStrLn $ "Unhandled frames:" ++ show nontextIDs
+
+    forM_ textFrames (\(header, bytes) -> do
+        putStr $ fst header ++ ": "
+        case parse (ID3.V2p2.parseTextFrame header) bytes of
+            Nothing -> putStrLn "Failed to parse"
+            Just result -> TIO.putStrLn result
+        )
+
+incrementalParse :: L.ByteString -> IO ()
+incrementalParse input = do
+    case try parseHeader of
+        Nothing -> putStrLn "Could not parse header"
+        Just header -> do
+            let flags = id3Flags header
+                version = id3Version header
+
+            putStr $ "Version " ++ show version ++ ", "
+            putStr $ "Flags: " ++ show flags ++ ". "
+
+            case try parseID3 of
+                Nothing -> putStrLn "Could not parse rest of file"
+                Just [] -> putStrLn "Unhandled version"
+                Just frames -> putStrLn "" >> handleFrames frames
+    where try x = parse x input
 
 someFunc file = do
     fileContents <- openFile file ReadMode >>= L.hGetContents
-    print $ parse parseID3 fileContents
 
-    case parse parseHeader fileContents of
-        Nothing -> return () --putStrLn $ file ++ ": Could not parse header"
-        Just header -> when (flags /= 0 && vers == 2) $ putStrLn $ file ++ ": " ++ show flags
-            where flags = id3Flags header
-                  vers  = fst $ id3Version header
+    putStr $ file ++ ": "
+
+    incrementalParse fileContents
--- src/ID3/V2p2.hs
+++ src/ID3/V2p2.hs
@@ -1,23 +1,27 @@
 module ID3.V2p2
     ( parseTag
     , FrameHeader
+    , UnparsedFrame
+    , parseTextFrame
     ) where
 
-import Control.Monad (guard, when, void)
+import Control.Arrow (first)
+import Control.Monad (guard)
 import Data.Bits (countTrailingZeros, shiftL, Bits(..))
 import Data.Char (isAsciiUpper, isDigit)
-import Data.Maybe (isJust)
-import Data.List (intercalate)
+import Data.Text.Encoding (decodeLatin1, decodeUtf16LEWith)
+import Data.Text.Encoding.Error (replace)
 import Data.Word (Word8)
+import qualified Data.ByteString as S
 import qualified Data.ByteString.Lazy as L
-import qualified Data.ByteString.Lazy.Char8 as L8
+import qualified Data.Text as T
 
 import ID3.Header
 import ID3.Unsynchronisation (deunsynchronise)
 import ParseBS
 
 type FrameHeader = (String, Int)
-type Frame = (FrameHeader, L.ByteString)
+type UnparsedFrame = (FrameHeader, L.ByteString)
 
 bytesToInteger :: [Word8] -> Int
 bytesToInteger bytes = sum . map (uncurry shiftL) $ zip (map fromIntegral $ reverse bytes) [0,8..]
@@ -36,33 +40,168 @@ parseFrameHeaderSize = do
 parseFrameHeader :: Parse FrameHeader
 parseFrameHeader = (,) <$> parseFrameHeaderID <*> parseFrameHeaderSize
 
-extractFrame :: Parse Frame
+extractFrame :: Parse UnparsedFrame
 extractFrame = do
     header@(_, size) <- parseFrameHeader
     frameData <- parseBytes size
     return (header, frameData)
 
--- Must have at least one frame
-parseTag :: ID3Header -> Parse String
+parsePadding :: Int -> Parse ()
+parsePadding n = bytes (replicate n 0) >> return ()
+
+-- Remove the remaining tag after the header. The remaining parse state should
+-- contain only MPEG data after this function
+parseTag :: ID3Header -> Parse [UnparsedFrame]
 parseTag tagHeader = do
     let size  = id3Size tagHeader
         flags = id3Flags tagHeader
         unsynchronisation = testBit flags 7
         compression       = testBit flags 6
 
-    -- Remaining bits should be 0
-    guard $ not compression && countTrailingZeros flags >= 6
+    -- Compression algorithm is not defined by the standard so we cannot unparse
+    -- compressed tags
+    guard $ not compression
+    -- All but first two flag bits should be zero
+    guard $ countTrailingZeros flags >= 6
 
+    -- Remove desynchronisation if present - this removes bytes from the input
     newSize <- if unsynchronisation then deunsynchronise size
                                     else return size
 
-    numBytesAfterHeader <- bsSize <$> look
+    
+    bytesAfterHeader <- bsSize <$> look
+    -- Extract list of one or more frames
     frames <- some extractFrame
-    bytesAfterFrames <- look
-    let numPaddingBytes = newSize - (numBytesAfterHeader - (bsSize bytesAfterFrames))
-        paddingBytes = L.take (fromIntegral numPaddingBytes) bytesAfterFrames
-    bytes $ replicate numPaddingBytes 0
-    return . show $ map (fst.fst) frames
+    bytesAfterFrames <- bsSize <$> look
+
+    -- If the number of frame bytes parsed is less than the tag size, either
+    -- the tag has 0 padding, we've failed to parse a valid frame, or the tag is
+    -- invalid.
+    let numFrameBytes = (bytesAfterHeader - bytesAfterFrames)
+        numPaddingBytes = newSize - numFrameBytes
+
+    -- Ensure all padding bytes are 0 - otherwise we've failed to parse something
+    parsePadding numPaddingBytes
+    return frames
 
 bsSize :: (Integral a) => L.ByteString -> a
 bsSize bs = fromIntegral $ L.length bs
+
+
+{- ------------------------------------------------------------------- -
+ - ID3 v2.2 Frames
+ - ------------------------------------------------------------------- -}
+
+{- 
+ - BUF
+ - CNT
+ - COM
+ - CRA
+ - CRM
+ - ETC
+ - EQU
+ - GEO
+ - IPL
+ - LNK
+ - MCI
+ - MLL
+ - PIC
+ - POP
+ - REV
+ - RVA
+ - SLT
+ - STC
+ - UFI
+ - ULT
+  
+ - URL frames
+ -
+ - WAF
+ - WAR
+ - WAS
+ - WCM
+ - WCP
+ - WPB
+
+ - Text frames
+ -
+ - TT1      Content group description             e.g.piano - concerto/weather-hurricane
+ - TT2      Title/Songname/Content description
+ - TT3      Subtitle/Description refinement       e.g. op16/performed live at wembley
+ - TP1      Lead artist/performer/soloist/group   separated by / character
+ - TP2      Band/Orchestra/Accompaniment
+ - TP3      Conductor
+ - TP4      Interpreted/remixed/modifed by
+ - TCM      Composer(s)                           separated by / character
+ - TXT      Lyricist/text writer
+ - TLA      Language(s)                           3 byte ISO-639-2 codes. Multiple languages are listed in order of appearance
+ - TCO      Content type                          (Numeric string. Refs to ID3v1 content type are wrapped in "(" ")".
+ -                                                Optionally followed by refinement e.g. (4)Eurodisco. Can mix several e.g. (51)(39).
+ -                                                If refinement should begin with (, starts with (( instead.
+ - TAL      Album/Movie/Show title
+ - TPA      Part of a set                         Numeric string optionally extended with "/". e.g. 1/2
+ - TRK      Track number                          Numeric string optionally extended with "/". e.g. 4/9
+ - TRC      ISRC                                  International Standard Recoding Code
+ - TYE      Year                                  4 character numeric string
+ - TDA      Date                                  4 character numeric string DDMM
+ - TIM      Time                                  4 character numeric string HHMM
+ - TRD      Recording dates                       e.g. "4th-7th June, 12 June"
+ - TMT      Media Type                            Textstring or reference to predefined types in "(" ")" similar to TCO
+ - TFT      File type                             MPG /1/2/3/2.5/AAC or other type
+ - TBP      BPM                                   Integral numeric string
+ - TCR      Copyright                             year and space (5 characters) followed by copyright of original piece of music
+ -                                                Must be displayed as "Copyright "(C)" "...
+ - TPB      Publisher
+ - TEN      Encoded by
+ - TSS      Software/hardware settings
+ - TOF      Original Filename
+ - TLE      Length                                Numeric string in ms
+ - TSI      Size                                  Numeric string in bytes excl. tag
+ - TDY      Playlist delay                        Numeric string in ms (silence between every song in a playlist)
+ - TKE      Initial key                           String with max len 3: [A-G][b#]?[mo]?
+ - TOT      Original album/movie/show
+ - TOA      Original artist(s)/performer(s)       Separated with /
+ - TOL      Original lyricist(s)
+ - TOR      Original release year                 As TDY
+ -
+ - TXX      User defined...   "TXX"
+ -          Frame size        $xx xx xx
+ -          Text encoding     $xx
+ -          Description       <textstring> $00 (00)
+ -          Value             <textstring>
+ -}
+
+data Encoding = Latin1 | Utf16 deriving Eq
+parseEncoding = toEncoding <$> satisfy (`elem` [0,1])
+    where toEncoding 0 = Latin1
+          toEncoding 1 = Utf16
+
+-- parseFrameContent :: FrameHeader
+textEncoder :: Encoding -> (S.ByteString -> T.Text)
+textEncoder Latin1 = decodeLatin1
+textEncoder Utf16  = decodeUtf16LEWith (replace '?')
+
+zeroTerminate :: Encoding -> S.ByteString -> (S.ByteString, S.ByteString)
+zeroTerminate Latin1 = S.break (/=0)
+zeroTerminate Utf16 = (first S.concat) . zeroTerminateUtf16
+
+-- TODO: This should fail if odd number of characters 
+zeroTerminateUtf16 :: S.ByteString -> ([S.ByteString], S.ByteString)
+zeroTerminateUtf16 s
+    | S.length s < 2 = ([S.empty], S.empty)
+    | char == zero   = ([S.empty], rest)
+    | otherwise      = first (char :) $ zeroTerminateUtf16 rest
+    where (char, rest) = S.splitAt 2 s
+          zero = S.pack [0,0]
+
+takeEveryOther [] = []
+takeEveryOther [x] = [x]
+takeEveryOther (x:_:xs) = x : takeEveryOther xs
+
+parseTextFrame :: FrameHeader -> Parse T.Text
+parseTextFrame (id, size) = do
+    encoding <- parseEncoding
+    str <- L.toStrict <$> parseBytes (size - 1)
+    -- 
+    guard $ encoding == Latin1 || even (S.length str)
+    return . textEncoder encoding . fst $ zeroTerminate encoding str
